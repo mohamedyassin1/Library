14fd3dae8ec22593d1deca3736bc4a9346fe091f
Mohamed Yassin <73013959+mohamedyassin1@users.noreply.github.com>
2022-02-23 12:47:58 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/public')));
+//public
+//css
+//js
+//imgs
+
+app.set('view engine', 'ejs'); //using the ejs view engine, so we can do dynamic HTML templating.
+app.set('views', path.join(__dirname, '/views')); //Add this so that we can run our app from any directory.
+
+//Home Page
+app.get('/', (req, res) => {
+    res.render('home'); //don't need to say views/home.ejs, as the default place to look is views.
+});
+//Listening for requests
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+})
