0f80fe8bd03748f35c79ba6845ebe53c95bac64e
Angelo Gonzales <angelo.gonzales@ucalgary.ca>
2022-02-23 14:24:32 -0700

    bunch of stuff
    index api stuff
    updated navbar included mysql in package

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: '34.83.176.15',
+    user: 'root',
+    password: 'tNe855bucM5wHojL',
+    database: 'librarydb'
+});
+
 app.use(express.static(path.join(__dirname, '/public')));
 //public
 //css
@@ -15,7 +23,30 @@ app.set('views', path.join(__dirname, '/views')); //Add this so that we can run 
 app.get('/', (req, res) => {
     res.render('home'); //don't need to say views/home.ejs, as the default place to look is views.
 });
+
+//api
+app.get('/api/books', (req, res) => {
+    connection.query('SELECT * FROM `Books`', function (error, results, fields) {
+        if (error) console.error('bad query!');
+        // connected!
+        console.log('The result is: ', results.length);
+        res.send(results[0]);
+    });
+    
+});
+
+connection.connect((err) => {
+    if(err){
+        console.error('error cannot conenct to db');
+        return;
+    }
+
+    console.log('connected to db');
+});
+
 //Listening for requests
 app.listen(3000, () => {
     console.log("listening on port 3000");
-})
+});
+
+
