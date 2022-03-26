ab65f4cacf4a582971755b2b042219f3c0fecc46
DaveSharma-Hub <davesharma12345@outlook.com>
2022-03-26 17:53:48 -0600

    Tests/Signup fixed

--- test/index.test.js
+++ test/index.test.js
@@ -32,10 +32,112 @@ describe('index.js test', () => {
     .end((err, res) => {
       expect(res).to.have.status(200);
       expect(res.body).to.be.a('array');
-      done();
-          
+      done();  
     });
-    
+  });
+
+  //Test HTML GET request returns an OK(200) status for login page
+  it('get test login', (done) => {
+    chai.request(app)
+    .get('/login')
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      // expect(res.body).to.be.a('array');
+      done();  
+    });
+  });
+
+  //Test can login with verified email, username and password
+  it('post test login', (done) => {
+    chai.request(app)
+    .post('/api/login')
+    .send({email: 'john@gmail.com', username:'john', password:'12345'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      // expect(res.body).to.be.a('array');
+      done();  
+    });
+  });
+
+  //Test rejects invalid email, by redirecting back to valid HTML page
+  it('post test login', (done) => {
+    chai.request(app)
+    .post('/api/login')
+    .send({email: 'kelly@gmail.com', username:'john', password:'12345'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      // expect(res.body).to.be.a('array');
+      done();  
+    });
+  });
+
+  //Test rejects invalid username, by redirecting back to valid HTML page
+  it('post test login', (done) => {
+    chai.request(app)
+    .post('/api/login')
+    .send({email: 'john@gmail.com', username:'kelly', password:'12345'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      // expect(res.body).to.be.a('array');
+      done();  
+    });
+  });
+
+  //Test sign-up page valid HTML OK(200) request
+  it('get test sign-up', (done) => {
+    chai.request(app)
+    .get('/signUp')
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      // expect(res.body).to.be.a('array');
+      done();  
+    });
+  });
+
+   //Test rejects already used username, by redirecting back to valid HTML page
+  it('post test sign-up', (done) => {
+    chai.request(app)
+    .post('/api/signUp')
+    .send({email: 'john@gmail.com', username:'john', password:'12345'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      // expect(res.body).to.be.a('array');
+      done();  
+    });
+  });
+
+  //Test logout redirection to HTML OK(200) Page
+  it('get test logout', (done) => {
+    chai.request(app)
+    .get('/logout')
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+
+  //Test valid admin email, by redirecting back to valid HTML page
+  it('post test login', (done) => {
+    chai.request(app)
+    .post('/api/login')
+    .send({email: 'admin@gmail.com', username:'admin', password:'12345'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      // expect(res.body).to.be.a('array');
+      done();  
+    });
+  });
+
+  //Test rejects invalid admin email, by redirecting back to valid HTML page
+  it('post test login', (done) => {
+    chai.request(app)
+    .post('/api/login')
+    .send({email: 'admin@gmail.com', username:'user', password:'12345'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      // expect(res.body).to.be.a('array');
+      done();  
+    });
   });
 
 });
